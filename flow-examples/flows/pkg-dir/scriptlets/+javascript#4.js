36b702dc24e1fcaa733fa98c2af8ef41ba549fae
leavesster <11785335+leavesster@users.noreply.github.com>
2025-04-08 16:44:31 +0800

    chore: add pkg dir test case

---
+++ flow-examples/flows/pkg-dir/scriptlets/+javascript#4.js
@@ -0,0 +1,24 @@
+//#region generated meta
+/**
+ * @typedef {{
+ *   input: string;
+ * }} Inputs
+ * @typedef {{
+ *   output: string;
+ * }} Outputs
+ */
+//#endregion
+
+import fs from "fs/promises";
+
+/**
+ * @param {Inputs} params
+ * @param {import("@oomol/types/oocana").Context<Inputs, Outputs>} context
+ * @returns {Promise<Outputs>}
+ */
+export default async function (params, context) {
+  let tmpPkg = context.pkgDir;
+  const ls = await fs.lstat(tmpPkg);
+
+  return { output: "output_value" };
+}
