6a304c0caaf923fbcc02ff4fb31ea8650e42bb0f
leavesster <11785335+leavesster@users.noreply.github.com>
2025-04-29 16:58:40 +0800

    feat: test slot example

---
+++ flow-examples/test/slot.test.ts
@@ -0,0 +1,38 @@
+import { it, expect } from "vitest";
+import { runFlow } from "./run";
+import { describe } from "node:test";
+
+describe("slot test", () => {
+  it("run slot-task flow", async () => {
+    const { code, events } = await runFlow("slot-task");
+    const e = events.filter(
+      e =>
+        e.event === "BlockFinished" &&
+        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
+    );
+    expect(e.length).toBe(1);
+    expect(code).toBe(0);
+  });
+
+  it("run slot-inline flow with bind", async () => {
+    const { code, events } = await runFlow("slot-inline");
+    const e = events.filter(
+      e =>
+        e.event === "BlockFinished" &&
+        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
+    );
+    expect(e.length).toBe(1);
+    expect(code).toBe(0);
+  });
+
+  it("run slot-subflow flow with bind", async () => {
+    const { code, events } = await runFlow("slot-subflow");
+    const e = events.filter(
+      e =>
+        e.event === "BlockFinished" &&
+        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
+    );
+    expect(e.length).toBe(1);
+    expect(code).toBe(0);
+  });
+});
