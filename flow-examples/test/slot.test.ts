74b6b9a2c2d8aaf3bc77fec44933c4f5e8d2ba6f
leavesster <11785335+leavesster@users.noreply.github.com>
2025-05-01 15:18:42 +0800

    feat: add package subflow test case

--- flow-examples/test/slot.test.ts
+++ flow-examples/test/slot.test.ts
@@ -36,3 +36,38 @@ describe("slot test", () => {
     expect(code).toBe(0);
   });
 });
+
+describe("package slot test", () => {
+  it("run pkg slot-task flow", async () => {
+    const { code, events } = await runFlow("slot-task");
+    const e = events.filter(
+      e =>
+        e.event === "BlockFinished" &&
+        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
+    );
+    expect(e.length).toBe(1);
+    expect(code).toBe(0);
+  });
+
+  it("run pkg slot-inline flow", async () => {
+    const { code, events } = await runFlow("slot-inline");
+    const e = events.filter(
+      e =>
+        e.event === "BlockFinished" &&
+        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
+    );
+    expect(e.length).toBe(1);
+    expect(code).toBe(0);
+  });
+
+  it("run pkg slot-subflow flow", async () => {
+    const { code, events } = await runFlow("slot-subflow");
+    const e = events.filter(
+      e =>
+        e.event === "BlockFinished" &&
+        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
+    );
+    expect(e.length).toBe(1);
+    expect(code).toBe(0);
+  });
+});
