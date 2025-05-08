151a1ccdd69f0ff5e96c442c100edc1f56d7f37e
leavesster <11785335+leavesster@users.noreply.github.com>
2025-05-08 16:32:35 +0800

    feat: throw error if flow node store exist variables

--- flow-examples/subflows/basic/scriptlets/+javascript#1.js
+++ flow-examples/subflows/basic/scriptlets/+javascript#1.js
@@ -15,5 +15,14 @@
  * @returns {Promise<Outputs>}
  */
 export default async function (params, context) {
+  const v1 = context.flowNodeStore["v1"];
+  const v2 = context.flowNodeStore["v2"];
+
+  if (v1 && v2) {
+    throw new Error("v1 and v2 already exist");
+  } else {
+    context.flowNodeStore["v1"] = "v1";
+    context.flowNodeStore["v2"] = "v2";
+  }
   return { output: params.input };
 }
