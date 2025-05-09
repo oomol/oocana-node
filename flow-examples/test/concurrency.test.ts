4fed2d9279eaaa042e50d1d417397e1c3c871333
leavesster <11785335+leavesster@users.noreply.github.com>
2025-05-09 23:05:39 +0800

    feat: add concurrency test

---
+++ flow-examples/test/concurrency.test.ts
@@ -0,0 +1,11 @@
+import { it, expect, describe } from "vitest";
+import { runFlow } from "./run";
+
+describe("concurrency test", () => {
+  it("run two flow", async () => {
+    const r = await Promise.all([runFlow("sub"), runFlow("pkg")]);
+    for (const { code, events } of r) {
+      expect(code).toBe(0);
+    }
+  });
+});
