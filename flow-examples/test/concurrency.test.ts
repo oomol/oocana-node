09b2aa793d7c946008b860a874def58ac4cebcb0
leavesster <11785335+leavesster@users.noreply.github.com>
2025-05-12 16:33:31 +0800

    chore: increase timeout (#113)

--- flow-examples/test/concurrency.test.ts
+++ flow-examples/test/concurrency.test.ts
@@ -1,11 +1,17 @@
 import { it, expect, describe } from "vitest";
 import { runFlow } from "./run";
 
-describe("concurrency test", () => {
-  it("run two flow", async () => {
-    const r = await Promise.all([runFlow("sub"), runFlow("pkg")]);
-    for (const { code, events } of r) {
-      expect(code).toBe(0);
-    }
-  });
-});
+describe(
+  "concurrency test",
+  () => {
+    it("run two flow", async () => {
+      const r = await Promise.all([runFlow("sub"), runFlow("pkg")]);
+      for (const { code, events } of r) {
+        expect(code).toBe(0);
+      }
+    });
+  },
+  {
+    timeout: 1000 * 20,
+  }
+);
