ca49d1c20f2501070f20956a4b65e2cc69bf3ab8
leavesster <11785335+leavesster@users.noreply.github.com>
2025-06-17 15:03:34 +0800

    feat: remove slot inline example

--- flow-examples/test/slot.test.ts
+++ flow-examples/test/slot.test.ts
@@ -14,45 +14,6 @@ describe("slot test", () => {
     expect(code).toBe(0);
   });
 
-  it("run slot-inline flow with bind", async () => {
-    const { code, events } = await runFlow("slot-inline");
-    const e = events.filter(
-      e =>
-        e.event === "BlockFinished" &&
-        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
-    );
-    expect(e.length).toBe(1);
-    expect(code).toBe(0);
-  });
-
-  it("run map-slot-inline flow", async () => {
-    const { code, events } = await runFlow("map-slot-inline");
-    expect(code).toBe(0);
-    const slotflowOutputs = events.filter(e => e.event === "SlotflowOutput");
-    expect(slotflowOutputs.length).toBe(2);
-    expect(
-      slotflowOutputs[0].data.output,
-      JSON.stringify(slotflowOutputs[0])
-    ).toEqual("aaa");
-
-    expect(
-      slotflowOutputs[1].data.output,
-      JSON.stringify(slotflowOutputs[1])
-    ).toEqual("bbb");
-
-    const subflowOutputs = events.filter(e => e.event === "SubflowBlockOutput");
-    expect(subflowOutputs.length, JSON.stringify(subflowOutputs)).toBe(2);
-    expect(
-      subflowOutputs[0].data.output,
-      JSON.stringify(subflowOutputs[0])
-    ).toEqual(["aaa", "bbb"]);
-
-    expect(
-      subflowOutputs[1].data.output,
-      JSON.stringify(subflowOutputs[1])
-    ).toEqual(["aaa", "bbb"]);
-  });
-
   it("run slot-subflow flow with bind", async () => {
     const { code, events } = await runFlow("slot-subflow");
     const e = events.filter(
@@ -88,17 +49,6 @@ describe("slotflow test", () => {
 describe("package slot test", () => {
   it("run pkg slot-task flow", async () => {
     const { code, events } = await runFlow("pkg-slot-task");
-    const e = events.filter(
-      e =>
-        e.event === "BlockFinished" &&
-        e.data.stacks.filter(e => e.node_id === "node-2").length == 1
-    );
-    expect(e.length).toBe(1);
-    expect(code).toBe(0);
-  });
-
-  it("run pkg slot-inline flow", async () => {
-    const { code, events } = await runFlow("pkg-slot-inline");
     const e = events.filter(
       e =>
         e.event === "BlockFinished" &&
