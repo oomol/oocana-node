2d766a2b65df0ffc5be8875837fb9beea3679f59
leavesster <11785335+leavesster@users.noreply.github.com>
2025-06-24 16:32:38 +0800

    chore: add nullable test case

---
+++ flow-examples/flows/nullable/scriptlets/+scriptlet#2.ts
@@ -0,0 +1,21 @@
+//#region generated meta
+type Inputs = {
+    input: any;
+    input1: any;
+};
+type Outputs = {
+    output: any;
+};
+//#endregion
+
+import type { Context } from "@oomol/types/oocana";
+
+export default async function(
+    params: Inputs,
+    context: Context<Inputs, Outputs>
+): Promise<Partial<Outputs> | undefined | void> {
+
+    // your code
+
+    return { output: "output_value" };
+};
