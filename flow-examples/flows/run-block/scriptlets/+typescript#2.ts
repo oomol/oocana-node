f3562d6f1d30d72a97ff6a34242cb5ee8403711e
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-10 14:08:58 +0800

    test: add timeout promise to avoid waiting

--- flow-examples/flows/run-block/scriptlets/+typescript#2.ts
+++ flow-examples/flows/run-block/scriptlets/+typescript#2.ts
@@ -13,14 +13,31 @@ export default async function (
   context: Context<Inputs, Outputs>
 ): Promise<Outputs> {
   const res = await context.runBlock("counter11", { input: "test" });
-  const data = await res.finish();
-  console.log("Result from counter block:", data);
-  if (data.error) {
-    console.error("Error in counter block:", data.error);
-  } else {
+
+  try {
+    const data: any = await Promise.race([
+      res.finish(),
+      new Promise((_, reject) =>
+        setTimeout(
+          () => reject(new Error("Timeout waiting for counter block11")),
+          5000
+        )
+      ),
+    ]);
+    console.log("Result from counter block:", data);
+    if (data.error) {
+      console.error("Error in counter block:", data.error);
+    } else {
+      throw new Error(
+        "Expected an error, but got result: " + JSON.stringify(data.result)
+      );
+    }
+  } catch (error) {
     throw new Error(
-      "Expected an error, but got result: " + JSON.stringify(data.result)
+      "run block timeout error: " +
+        (error instanceof Error ? error.message : String(error))
     );
   }
+
   return { out: "out" };
 }
