942eabb9a08b72c4d4a1de27231fbecf245b767c
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-14 16:39:07 +0800

    test: add package flow test case

--- flow-examples/flows/run-pkg-block/scriptlets/+typescript#2.ts
+++ flow-examples/flows/run-pkg-block/scriptlets/+typescript#2.ts
@@ -12,27 +12,31 @@ export default async function (
   _inputs: Inputs,
   context: Context<Inputs, Outputs>
 ): Promise<Outputs> {
-  const res = await context.runBlock("counter11", {
-    inputs: { input: "test" },
+  const res = await context.runBlock("sub::basic", {
+    inputs: { input: "111" },
+  });
+  res.onOutput(data => {
+    const { handle, value } = data;
+    console.log(
+      `Received output from counter block: handle=${handle}, output=${value}`
+    );
   });
 
   try {
-    const data: any = await Promise.race([
+    const resolve: any = await Promise.race([
       res.finish(),
       new Promise((_, reject) =>
         setTimeout(
-          () => reject(new Error("Timeout waiting for counter block11")),
+          () => reject(new Error("Timeout waiting for counter block")),
           5000
         )
       ),
     ]);
-    console.log("Result from counter block:", data);
-    if (data.error) {
-      console.error("Error in counter block:", data.error);
+    const { result, error } = resolve;
+    if (error) {
+      throw new Error("Counter block failed with error: " + error);
     } else {
-      throw new Error(
-        "Expected an error, but got result: " + JSON.stringify(data.result)
-      );
+      console.log("Result from counter block:", result);
     }
   } catch (error) {
     throw new Error(
@@ -40,6 +44,5 @@ export default async function (
         (error instanceof Error ? error.message : String(error))
     );
   }
-
   return { out: "out" };
 }
