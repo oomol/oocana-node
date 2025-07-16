710176310658e4dc09e3a91d290afccec45aa4d6
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-16 14:56:48 +0800

    test: add object check

--- flow-examples/flows/query-downstream/scriptlets/+typescript#1.ts
+++ flow-examples/flows/query-downstream/scriptlets/+typescript#1.ts
@@ -1,5 +1,5 @@
 import type { Context } from "@oomol/oocana-types";
-
+import { dequal } from "dequal";
 type Inputs = {
   in: unknown;
 };
@@ -13,7 +13,24 @@ export default async function (
   context: Context<Inputs, Outputs>
 ): Promise<Outputs> {
   const downstream = await context.queryDownstream();
-  console.log("downstream", downstream);
+
+  let eq = dequal(downstream["a"], {
+    to_node: [
+      {
+        node_id: "end",
+        input_handle: "a",
+        input_handle_def: {
+          handle: "a",
+        },
+      },
+    ],
+  });
+
+  if (!eq) {
+    throw new Error(
+      "Downstream query did not return expected value for 'a'" + downstream["a"]
+    );
+  }
 
   return { a: "a", b: "b" };
 }
