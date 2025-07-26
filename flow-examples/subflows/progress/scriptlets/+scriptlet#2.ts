7a10506b8d98ea444e61948a73992db914c3b288
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-26 11:42:50 +0800

    test: add subflow progress

---
+++ flow-examples/subflows/progress/scriptlets/+scriptlet#2.ts
@@ -0,0 +1,23 @@
+//#region generated meta
+type Inputs = {
+    output: any;
+};
+type Outputs = {
+    output: any;
+};
+//#endregion
+
+import type { Context } from "@oomol/types/oocana";
+
+export default async function(
+    params: Inputs,
+    context: Context<Inputs, Outputs>
+): Promise<Partial<Outputs> | undefined | void> {
+
+    for (let i = 0; i < 4; i++) {
+        await new Promise(r => setTimeout(r, 1000));
+        context.reportProgress(i * 25)
+    }
+
+    return { output: "output_value" };
+};
