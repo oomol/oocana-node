161ce6e516d153c54a0c46cc9fe33b454a05696b
leavesster <11785335+leavesster@users.noreply.github.com>
2025-08-14 14:17:05 +0800

    refactor: reduce same logic code (#347)
    * refactor: add type
    
    * refactor: extract projectData pkgDataRoot
    
    * refactor: extract env, bind and path and exclude
    
    * refactor: extract session id
    
    * refactor: extract debug
    
    * refactor: merge config
    
    * fix: remove handle session id

--- packages/oocana/src/oocana.ts
+++ packages/oocana/src/oocana.ts
@@ -10,16 +10,18 @@ import { generateSpawnEnvs, SpawnedEnvs } from "./env";
 
 export type JobEvent = Remitter<OocanaEventConfig>;
 
-export interface RunBlockConfig {
-  /** block.yaml file path or directory path */
-  blockPath: string;
+interface BaseConfig {
+  /** optional session id, if not give, oocana will generate one */
   sessionId?: string;
+
+  /** Debug mode. If enable, when oocana spawn executor it will give some debugging message to every executor to make they support debugging. Only support in python-executor and nodejs-executor now */
   debug?: boolean;
-  inputs?: {
-    [handleId: string]: any;
-  };
-  /** search package blocks's path */
-  searchPaths?: [];
+
+  /** A directory that can be used for persistent data storage. Flows and blocks that are not part of a package will use this directory */
+  projectData: string;
+  /** a directory that can be used for persistent package data, all package's data will store in this directory. it can persist across sessions */
+  pkgDataRoot: string;
+
   /** exclude packages, these package will not use ovm layer feature if the feature is enabled */
   excludePackages?: string[];
   /** a path for session storage. this path will shared by all block by context.sessionDir or context.session_dir */
@@ -41,25 +43,29 @@ export interface RunBlockConfig {
    * when oocana spawn. If not given oocana will search OOCANA_BIND_PATH_FILE to see if it has one.
    * */
   envFile?: string;
-  /** A directory that can be used for persistent data storage. Flows and blocks that are not part of a package will use this directory */
-  projectData: string;
-  /** a directory that can be used for persistent package data, all package's data will store in this directory. it can persist across sessions */
-  pkgDataRoot: string;
 }
 
-export interface RunFlowConfig {
+export interface RunBlockConfig extends BaseConfig {
+  /** block.yaml file path or directory path */
+  blockPath: string;
+  inputs?: {
+    [handleId: string]: any;
+  };
+  /** search package blocks's path */
+  searchPaths?: string[];
+}
+
+export interface RunFlowConfig extends BaseConfig {
   /** flow.yaml file path or directory path */
   flowPath: string;
   /** comma separated paths for search block package */
   searchPaths?: string;
-  /** optional session id */
-  sessionId?: string;
+
   /** address for mqtt */
   address?: string;
   /** will use previous run's last input value, but only json value can be reused */
   useCache?: boolean;
-  /** Debug mode. If enable, when oocana spawn executor it will give some debugging message to every executor to make they support debugging. Only support in python-executor and nodejs-executor now */
-  debug?: boolean;
+
   /** only run these nodes */
   nodes?: string[];
   /** @deprecated use nodesInputs instead */
@@ -75,28 +81,6 @@ export interface RunFlowConfig {
   };
   /** @deprecated use nodes parameter instead */
   toNode?: string;
-  /** exclude packages, these package will not use ovm layer feature if the feature is enabled */
-  excludePackages?: string[];
-  /** a path for session storage. this path will shared by all block by context.sessionDir or context.session_dir */
-  sessionPath?: string;
-  /** a temporary root directory for session storage. oocana will create a subdirectory for each flowPath (one flow path always has the same subdirectory name). oocana will clean this subdirectory after flow session finish success but retain if the session is not successful. every block can get this subdirectory from context.tmpDir or context.tmp_dir. */
-  tempRoot?: string;
-  /** bind paths, format  src=<source>,dst=<destination>,[ro|rw],[nonrecursive|recursive], oocana will mount source to target in layer. if target not exist, oocana will create it. */
-  bindPaths?: string[];
-  /** a file path contains multiple bind paths, better use absolute path. The file format is src=<source>,dst=<destination>,[ro|rw],[nonrecursive|recursive] line by line, if not provided, it will be found in OOCANA_BIND_PATH_FILE env variable */
-  bindPathFile?: string;
-  /** default is process.env */
-  spawnedEnvs?: SpawnedEnvs;
-  /** Environment variables passed to all executors. All variable names will be converted to uppercase; then if the variable name does not start with OOMOL_, the OOMOL_ prefix will be added automatically. */
-  oomolEnvs?: Record<string, string>;
-  /** when spawn executor, oocana will only retain envs start with OOMOL_ by design. Other env variables need to be explicitly added in this parameters otherwise they will be ignored. */
-  envs?: Record<string, string>;
-  /** .env file path, better use absolute path, format should be <key>=<value> line by line. These variables will pass to executor when oocana spawn. If not given oocana will search OOCANA_BIND_PATH_FILE to see if it has one. */
-  envFile?: string;
-  /** A directory that can be used for persistent data storage. Flows and blocks that are not part of a package will use this directory */
-  projectData: string;
-  /** a directory that can be used for persistent package data, all package's data will store in this directory. it can persist across sessions */
-  pkgDataRoot: string;
 }
 
 export const DEFAULT_PORT = 47688;
@@ -106,6 +90,73 @@ export interface OocanaInterface {
   runFlow(config: RunFlowConfig): Promise<Cli>;
   runBlock(config: RunBlockConfig): Promise<Cli>;
   stop(sessionId: string): Promise<void>;
+}
+
+function buildArgs({
+  sessionId,
+  debug,
+  projectData,
+  pkgDataRoot,
+  excludePackages,
+  sessionPath,
+  tempRoot,
+  bindPaths,
+  bindPathFile,
+  envs,
+  envFile,
+}: BaseConfig): string[] {
+  const args: string[] = [];
+  if (sessionId) {
+    args.push("--session", sessionId);
+  }
+  if (debug) {
+    args.push("--debug");
+  }
+  if (projectData) {
+    args.push("--project-data", projectData);
+  }
+  if (pkgDataRoot) {
+    args.push("--pkg-data-root", pkgDataRoot);
+  }
+  if (excludePackages) {
+    args.push("--exclude-packages", excludePackages.join(","));
+  }
+  if (sessionPath) {
+    args.push("--session-dir", sessionPath);
+  }
+  if (tempRoot) {
+    args.push("--temp-root", tempRoot);
+  }
+
+  if (bindPaths) {
+    const pathPattern =
+      /^src=([^,]+),dst=([^,]+)(?:,(?:ro|rw))?(?:,(?:nonrecursive|recursive))?$/;
+
+    for (const path of bindPaths) {
+      if (!pathPattern.test(path)) {
+        throw new Error(
+          `Invalid bind path format: ${path}. Expected format: src=<source>,dst=<destination>,[ro|rw],[nonrecursive|recursive]`
+        );
+      }
+      args.push("--bind-paths", path);
+    }
+  }
+
+  if (bindPathFile) {
+    args.push("--bind-path-file", bindPathFile);
+  }
+
+  if (envFile) {
+    args.push("--env-file", envFile);
+  }
+
+  if (envs) {
+    for (const [key, _] of Object.entries(envs)) {
+      args.push("--retain-env-keys", key);
+    }
+  }
+
+  return args;
 }
 
 export class Oocana implements IDisposable, OocanaInterface {
@@ -137,73 +188,28 @@ export class Oocana implements IDisposable, OocanaInterface {
     return this;
   }
 
-  public async runBlock({
-    blockPath,
-    sessionId,
-    inputs,
-    searchPaths,
-    excludePackages,
-    sessionPath,
-    tempRoot,
-    debug,
-    bindPaths,
-    bindPathFile,
-    spawnedEnvs,
-    oomolEnvs,
-    envs,
-    envFile,
-    projectData,
-    pkgDataRoot,
-  }: RunBlockConfig): Promise<Cli> {
+  public async runBlock(blockConfig: RunBlockConfig): Promise<Cli> {
     if (!this.#address) {
       throw new Error("Cannot run flow without connecting to a broker");
     }
 
+    const {
+      blockPath,
+      inputs,
+      envs,
+      oomolEnvs,
+      spawnedEnvs,
+      searchPaths,
+      sessionId,
+    } = blockConfig;
+
     const args = ["run", blockPath, "--reporter", "--broker", this.#address];
+    const baseArgs = buildArgs(blockConfig);
 
-    if (projectData) {
-      args.push("--project-data", projectData);
-    }
+    args.push(...baseArgs);
 
-    if (pkgDataRoot) {
-      args.push("--pkg-data-root", pkgDataRoot);
-    }
-
-    if (sessionId) {
-      args.push("--session", sessionId);
-    }
     if (inputs) {
       args.push("--inputs", JSON.stringify(inputs));
-    }
-
-    if (excludePackages) {
-      args.push("--exclude-packages", excludePackages.join(","));
-    }
-
-    if (sessionPath) {
-      args.push("--session-dir", sessionPath);
-    }
-
-    if (debug) {
-      args.push("--debug");
-    }
-
-    const pathPattern =
-      /^src=([^,]+),dst=([^,]+)(?:,(?:ro|rw))?(?:,(?:nonrecursive|recursive))?$/;
-
-    if (bindPaths) {
-      for (const path of bindPaths) {
-        if (!pathPattern.test(path)) {
-          throw new Error(
-            `Invalid bind path format: ${path}. Expected format: src=<source>,dst=<destination>,[ro|rw],[nonrecursive|recursive]`
-          );
-        }
-        args.push("--bind-paths", path);
-      }
-    }
-
-    if (tempRoot) {
-      args.push("--temp-root", tempRoot);
     }
 
     const executorEnvs = generateSpawnEnvs(envs, oomolEnvs, spawnedEnvs);
@@ -214,16 +220,8 @@ export class Oocana implements IDisposable, OocanaInterface {
       }
     }
 
-    if (envFile) {
-      args.push("--env-file", envFile);
-    }
-
     if (searchPaths) {
       args.push("--search-paths", searchPaths.join(","));
-    }
-
-    if (bindPathFile) {
-      args.push("--bind-path-file", bindPathFile);
     }
 
     const spawnedProcess = spawn(this.#bin, args, {
@@ -265,48 +263,31 @@ export class Oocana implements IDisposable, OocanaInterface {
     return flowTask;
   }
 
-  public async runFlow({
-    flowPath,
-    searchPaths,
-    sessionId,
-    nodes,
-    toNode,
-    useCache,
-    debug,
-    inputValues,
-    nodesInputs,
-    excludePackages,
-    sessionPath,
-    tempRoot,
-    bindPaths,
-    bindPathFile,
-    spawnedEnvs,
-    oomolEnvs,
-    envFile,
-    envs,
-    projectData,
-    pkgDataRoot,
-  }: RunFlowConfig): Promise<Cli> {
+  public async runFlow(flowConfig: RunFlowConfig): Promise<Cli> {
     if (!this.#address) {
       throw new Error("Cannot run flow without connecting to a broker");
     }
 
+    const {
+      flowPath,
+      searchPaths,
+      toNode,
+      nodesInputs,
+      inputValues,
+      useCache,
+      envs,
+      oomolEnvs,
+      spawnedEnvs,
+      sessionId,
+    } = flowConfig;
+    let nodes = flowConfig.nodes;
+
     const args = ["run", flowPath, "--reporter", "--broker", this.#address];
+    const baseArgs = buildArgs(flowConfig);
+    args.push(...baseArgs);
 
     if (searchPaths) {
       args.push("--search-paths", searchPaths);
-    }
-
-    if (projectData) {
-      args.push("--project-data", projectData);
-    }
-
-    if (pkgDataRoot) {
-      args.push("--pkg-data-root", pkgDataRoot);
-    }
-
-    if (sessionId) {
-      args.push("--session", sessionId);
     }
 
     if (toNode) {
@@ -327,50 +308,12 @@ export class Oocana implements IDisposable, OocanaInterface {
       args.push("--use-cache");
     }
 
-    if (excludePackages) {
-      args.push("--exclude-packages", excludePackages.join(","));
-    }
-
-    if (sessionPath) {
-      args.push("--session-dir", sessionPath);
-    }
-
-    if (debug) {
-      args.push("--debug");
-    }
-
-    const pathPattern =
-      /^src=([^,]+),dst=([^,]+)(?:,(?:ro|rw))?(?:,(?:nonrecursive|recursive))?$/;
-
-    if (bindPaths) {
-      for (const path of bindPaths) {
-        if (!pathPattern.test(path)) {
-          throw new Error(
-            `Invalid bind path format: ${path}. Expected format: src=<source>,dst=<destination>,[ro|rw],[nonrecursive|recursive]`
-          );
-        }
-        args.push("--bind-paths", path);
-      }
-    }
-
-    if (tempRoot) {
-      args.push("--temp-root", tempRoot);
-    }
-
     const executorEnvs = generateSpawnEnvs(envs, oomolEnvs, spawnedEnvs);
 
     if (envs) {
       for (const [key, _] of Object.entries(envs)) {
         args.push("--retain-env-keys", key);
       }
-    }
-
-    if (envFile) {
-      args.push("--env-file", envFile);
-    }
-
-    if (bindPathFile) {
-      args.push("--bind-path-file", bindPathFile);
     }
 
     const spawnedProcess = spawn(this.#bin, args, {
